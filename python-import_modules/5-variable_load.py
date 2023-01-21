7b5d412f11212a3d2812e80394f6b3ead9e8b405
AlExito745 <alxtinoco1988@gmail.com>
2023-01-21 09:00:25 -0800

    import varible from module

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Executing module as script
+if __name__ == "__main__":
+
+    # import variable "a" from module
+    from variable_load_5 import a
+
+    # print variable value
+    print("{}".format(a))
