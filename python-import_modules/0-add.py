75b4027d0dfc56ce44e792ce082160ef06a6c408
AlExito745 <alxtinoco1988@gmail.com>
2023-01-20 18:35:18 -0800

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-# importing add from add_0
-from add_0 import add
-# defining variables
-a = 1
-b = 2
-# calling function
-add(a, b)
-print("{} + {} = {}".format(a, b, add(a, b)), end="\n")
+# Executing modules as script
+if __name__ == "__main__":
+
+    # importing add from add_0
+    from add_0 import add
+
+    # defining variables
+    a = 1
+    b = 2
+
+    # calling function
+    add(a, b)
+    print("{} + {} = {}".format(a, b, add(a, b)), end="\n")
