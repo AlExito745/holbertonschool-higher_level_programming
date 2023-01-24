4994faae99200b4f40227fd77d84cb8d671f1685
AlExito745 <alxtinoco1988@gmail.com>
2023-01-24 11:57:05 -0800

    fix: correction code

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 # retrive an element from a list
 def element_at(my_list, idx):
-
-    for idx in range(0, (len(my_list) - 1)):
-        if idx < 0:
-            return(None)
-        elif idx > (len(my_list) - 1):
-            return(None)
+    if idx < 0 or idx > (len(my_list) - 1):
+        return(None)
     return(my_list[idx])
