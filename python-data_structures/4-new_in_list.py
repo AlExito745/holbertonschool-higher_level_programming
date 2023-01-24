808ebd507a31214df3d90b5ba3e07f16b9c5dbe2
AlExito745 <alxtinoco1988@gmail.com>
2023-01-24 14:54:48 -0800

    replace an element in a list without modifying the original

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# replace an element in a list without modifying the original
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > (len(my_list) - 1):
+        return(my_list)
+
+    copy = [i for i in my_list]
+    copy[idx] = element
+    return(copy)
