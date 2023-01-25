ba4652b7bc00e24c9c1788e8cef31f6ddc039c59
AlExito745 <alxtinoco1988@gmail.com>
2023-01-25 14:51:46 -0800

    fix: remove line

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# print a matrix of integers
 def print_matrix_integer(matrix=[[]]):
-
+    # print a matrix of integers
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
                 print("{:d}".format(matrix[i][j]), end="")
