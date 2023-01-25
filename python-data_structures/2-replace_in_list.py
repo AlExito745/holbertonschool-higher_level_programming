355154e48c02406e6662e3bc8bda7f2a9fb80419
AlExito745 <alxtinoco1988@gmail.com>
2023-01-25 07:17:17 -0800

    fix: remove line

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-# replace an element of a list at a specific position
-
+#!/usr/bin/python
 def replace_in_list(my_list, idx, element):
 
     if idx >= 0 and idx < (len(my_list)):
