412f93e06f7ef0a914a75c2205270ee3aad8885a
AlExito745 <alxtinoco1988@gmail.com>
2023-01-25 14:48:16 -0800

    fix: remove line

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# adds two tuples
 def multiple_returns(sentence):
-
+    # adds two tuples
     if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
