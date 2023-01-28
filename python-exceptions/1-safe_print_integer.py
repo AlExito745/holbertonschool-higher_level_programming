8e11610bb37e646402cefa82176f8db57ab1c6ed
AlExito745 <alxtinoco1988@gmail.com>
2023-01-28 13:57:04 -0800

    fix: remove a line

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     # function that print an integer using string format
-    if isinstance(value, (int, float, str, list, tuple, dict, set, None)):
-        try:
-            print("{:d}".format(value))
-            return(True)
-        except(TypeError, ValueError):
-            return(False)
+    try:
+        print("{:d}".format(value))
+        return(True)
+    except(TypeError, ValueError):
+        return(False)
