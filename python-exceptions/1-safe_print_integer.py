00319b4743bed61611aa9495e0830ddb08928382
AlExito745 <alxtinoco1988@gmail.com>
2023-01-28 13:53:19 -0800

    fix: add object of type

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     # function that print an integer using string format
-    if isinstance(value, (int, float, str, list, tuple, dict, set)):
+    if isinstance(value, (int, float, str, list, tuple, dict, set, None)):
         try:
             print("{:d}".format(value))
             return(True)
