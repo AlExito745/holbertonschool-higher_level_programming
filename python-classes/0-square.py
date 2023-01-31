2a943067315b5136def7d5956a73aece9ac7e6b1
AlExito745 <alxtinoco1988@gmail.com>
2023-01-31 11:32:32 -0800

    Fix: Correction docstrings

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+# 0-square.py
+# Alex Nuñez <5694@holbertonstudents.com>
+"""Define a class Square."""
+
+
 class Square:
-# Create an empty class with no attributes or methods.
+    """Represent a square."""
     pass
