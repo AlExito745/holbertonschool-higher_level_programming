2bd6af7bd21da52e1cc31be17de285bdc01b5af0
AlExito745 <alxtinoco1988@gmail.com>
2023-02-08 21:01:28 -0800

    fix: add a blank line

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 # Alex Nuñez <5694@holbertonstudents.com>
 """Defines an object attribute lookup function."""
 
+
 def lookup(obj):
     """Returns the list of available attributes
         and methods of an object
