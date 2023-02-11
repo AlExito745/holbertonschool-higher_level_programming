d331bfb84a4f0f01596b8b1c486113eee6e24c4a
AlExito745 <alxtinoco1988@gmail.com>
2023-02-10 20:33:13 -0800

    fix: remove a slice code

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,5 +8,5 @@
 def save_to_json_file(my_obj, filename):
     """Implementation of the JSON representation."""
     import json
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w") as f:
         json.dump(my_obj, f)
