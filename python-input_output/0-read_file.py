21ccacf5c94dcf9f71d95889a1945e11f8ee5a0e
AlExito745 <alxtinoco1988@gmail.com>
2023-02-10 16:09:13 -0800

    Fix: modified code

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+# 0-read_file.py
+# Alex Nuñez <5694@holbertonstudents.com>
 """Define a function that read a text file and print it to stdout."""
 
 
 def read_file(filename=""):
     """Implementation of the function."""
-    with open('my_file_0.txt', encoding="utf-8") as f:
-        print(f.read())
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
