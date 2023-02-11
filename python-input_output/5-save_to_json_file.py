89723dafe84501989cee1924a2aac756cabc283e
AlExito745 <alxtinoco1988@gmail.com>
2023-02-10 19:13:23 -0800

    fix: remove blank line

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,7 +7,6 @@
 
 def save_to_json_file(my_obj, filename):
     """Implementation of the JSON representation."""
-    
     import json
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
