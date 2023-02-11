a798c9b86b0458092661407b52550f4a40a2623c
AlExito745 <alxtinoco1988@gmail.com>
2023-02-10 19:11:26 -0800

    write an object to tex file from JSON representation

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 5-save_to_json_file.py
+# Alex Nuñez <5694@holbertonstudents.com>
+"""Define a function that writes an object to a tex file
+    using a JSON representation."""
+
+
+def save_to_json_file(my_obj, filename):
+    """Implementation of the JSON representation."""
+    
+    import json
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
