0129d464950225445e69f1854efa111b27ec01fa
AlExito745 <alxtinoco1988@gmail.com>
2023-02-14 13:54:05 -0800

    fix: change condition

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -33,11 +33,11 @@ class Rectangle(Base):
         @property
         def width(self):
             """Set/get the width of the Rectangle."""
-            return self.__width
+            return(self.__width)
 
         @width.setter
         def width(self, value):
-            if type(value) != int:
+            if type(value) is not int:
                 raise TypeError("width must be an integer")
             if width <= 0:
                 raise ValueError("width must be > 0")
@@ -46,11 +46,11 @@ class Rectangle(Base):
         @property
         def height(self):
             """Set/get the height of the Rectangle."""
-            return self.__height
+            return(self.__height)
 
         @height.setter
         def height(self, value):
-            if type(value) != int:
+            if type(value) is not int:
                 raise TypeError("height must be an integer")
             if height <= 0:
                 raise ValueError("height must be > 0")
@@ -59,11 +59,11 @@ class Rectangle(Base):
         @property
         def x(self):
             """Set/get the x coordinate of the Rectangle."""
-            return self.__x
+            return(self.__x)
 
         @x.setter
         def x(self, value):
-            if type(value) != int:
+            if type(value) is not int:
                 raise TypeError("x must be an integer")
             if x < 0:
                 raise ValueError("x must be >= 0")
@@ -72,11 +72,11 @@ class Rectangle(Base):
         @property
         def y(self):
             """Set/get the y coordinate of the Rectangle."""
-            return self.__y
+            return(self.__y)
 
         @y.setter
         def y(self, value):
-            if type(value) != int:
+            if type(value) is not int:
                 raise TypeError("y must be an integer")
             if y < 0:
                 raise ValueError("y must be >= 0")
