66403498d4d9ce3bebdc4d3210d614300fced5d1
AlExito745 <alxtinoco1988@gmail.com>
2023-03-09 09:16:12 -0800

    feat: update stament

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Create a first table in database MySQL Server.
-CREATE TABLE IF NOT EXISTS first_table (
-		id INT not null,
-		name VARCHAR(256) not null,
+CREATE TABLE IF NOT EXISTS "first_table" (
+		"id" INT not null,
+		"name" VARCHAR(256) not null,
 		);
