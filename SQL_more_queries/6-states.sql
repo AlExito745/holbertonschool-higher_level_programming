f9ea449c9868cda69ebfff2c1df3c1e5d574062c
AlExito745 <alxtinoco1988@gmail.com>
2023-03-10 14:35:48 -0800

    refactor: add path of table with dot notation on line 2

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Create a database and table on MySQL Server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 		`id` INT UNIQUE NOT NULL AUTO_INCREMENT,
 		`name` VARCHAR(256) NOT NULL,
 		PRIMARY KEY (`id`));
