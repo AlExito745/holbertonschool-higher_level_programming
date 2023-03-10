7025cd2b1790577b9a5ed345ad6d2f3afa57b736
AlExito745 <alxtinoco1988@gmail.com>
2023-03-10 14:00:02 -0800

    style: remove backtick and add token

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
 -- Create a table on MySQL Server.
-CREATE TABLE IF NOT EXISTS `id_not_null` (
+CREATE TABLE IF NOT EXISTS id_not_null (
 		`id`= 1 INT NOT NULL,
-		`name` VARCHAR(256);
+		`name` VARCHAR(256));
