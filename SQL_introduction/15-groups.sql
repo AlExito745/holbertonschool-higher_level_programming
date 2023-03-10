324b38525a2eaf4f8a4779db5036b84c4b3ec7e2
AlExito745 <alxtinoco1988@gmail.com>
2023-03-09 19:19:34 -0800

    refactor: change the code to resolve execution errors

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- List the number of records with the same value in a table of a database of MySQL Server.
 SELECT `score`, COUNT(*) AS `number`
 FROM `second_table`
-ORDER BY `score` DESC
-LIMIT 10;
+GROUP BY `score`
+ORDER BY `number` DESC;
