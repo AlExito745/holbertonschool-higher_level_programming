cb59d35b8f4792e7d06073f8292eac8d7c65a038
AlExito745 <alxtinoco1988@gmail.com>
2023-03-09 16:51:19 -0800

    feat: order staments

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
 -- List all records with a score >= 10 in a table of database of MySQL Server.
 SELECT `score`, `name`
 FROM `second_table`
-ORDER BY `score` DESC
 WHERE `score` >= 10
+ORDER BY `score` DESC
 LIMIT 10;
