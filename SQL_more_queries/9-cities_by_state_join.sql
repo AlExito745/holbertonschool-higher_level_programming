63b4e31ed0cbef506e61cd38e3862d29dc7790f4
AlExito745 <alxtinoco1988@gmail.com>
2023-03-10 17:04:03 -0800

    refactor: change all code

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- List all cities of a database in MySQL Server.
-SELECT `id`, `name`
-FROM `cities` AND SELECT (
-		`name`
-		FROM `states`)
-ORDER BY `id`;
+SELECT cities.`id`, cities.`name`, state.`name`
+FROM `cities` AS c
+INNER JOIN `states` AS s
+ON cities.`state_id` = state.`id`
+ORDER BY cities.`id`;
