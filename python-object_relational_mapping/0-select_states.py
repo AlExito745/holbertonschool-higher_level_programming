ea0bae352bf07e5e2cf4321395f0d1b28c35f319
AlExito745 <alxtinoco1988@gmail.com>
2023-03-13 14:12:07 -0700

    refactor: add new lines code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-# 0-select_states.py
 # Alex Nuñez <5694@holbertonstudents.com>
-"""List all states from the database hbtn_0e_0_usa.
-Arguments:
-username: the name user
-password: the password of the user
-database name: the name database
-"""
-
+# 0-select_states.py
+# List all states from the database hbtn_0e_0_usa.
+# Arguments:
+# <mysqlusername>
+# <mysql password>
+# <mysql database name>
+    
 
 import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect("username", "password", "databasename")
+    db = MySQLdb.connect(user="mysql username", passwd="mysql password", db="database name")
     c = db.cursor()
-    c.execute("SELECT `states` FROM `hbtn_0e_0_usa`
-              ORDER BY `s.id`")
-    print("({}, {})".format(`s.id`, `states`))
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    dt = c.fetchall()
+    for row in dt:
+        print(row)
