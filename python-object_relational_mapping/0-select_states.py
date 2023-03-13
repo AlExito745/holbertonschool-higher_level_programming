4173db325a4af24c7ca871b24dddeb8028f9c6b2
AlExito745 <alxtinoco1988@gmail.com>
2023-03-13 15:13:34 -0700

    refactor: change all code

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,6 +10,4 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
-    result = c.fetchall
-    for data in result:
-        print(data)
+    [print(state) for state in c.fetchall()]
