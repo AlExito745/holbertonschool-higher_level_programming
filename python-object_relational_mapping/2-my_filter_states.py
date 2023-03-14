1dcdb6424bad5188000c57e958bad45aefb46fbb
AlExito745 <alxtinoco1988@gmail.com>
2023-03-14 16:39:24 -0700

    refactor: add line code

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,8 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    query = "SELECT * FROM `states` WHERE BINARY `name` = '{}'".format(sys.argv[4])
+    query = "SELECT * FROM `states`"
+    query += "WHERE BINARY `name` = '{}'".format(sys.argv[4])
     c.execute(query)
     result = c.fetchall()
     for data in result:
