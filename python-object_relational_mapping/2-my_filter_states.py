5f905b709855f99d682b4f65b92c815524d5649d
AlExito745 <alxtinoco1988@gmail.com>
2023-03-14 16:17:08 -0700

    refactor: addprotector of run script

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,12 +12,14 @@ Arguments:
 import sys
 import MySQLdb
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-c = db.cursor()
-query = "SELECT * FROM `states` WHERE `name` = '{}'".format(sys.argv[4])
-c.execute(query)
-result = c.fetchall()
-for data in result:
-    print(data)
-c.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    query = "SELECT * FROM `states`"
+    c.execute(query)
+    result = c.fetchall()
+    for data in result:
+        if sys.argv[4] == data[1]:
+            print(data)
+    c.close()
+    db.close()
