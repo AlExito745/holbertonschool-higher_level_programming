176c7c96743ce6269f282ca30f96659901606d24
AlExito745 <alxtinoco1988@gmail.com>
2023-03-14 13:40:13 -0700

    style: modified the comment

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Displays all values in the states table of the database hbtn_0e_0_usa
 whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py
 Arguments:
 <mysql username>
 <mysql password>
