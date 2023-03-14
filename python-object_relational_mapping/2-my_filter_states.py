7ca802bd2ace80e1cb7cb0da491500948cbfdcc0
AlExito745 <alxtinoco1988@gmail.com>
2023-03-14 14:05:09 -0700

    refactor: change line 17

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Usage: ./2-my_filter_states.py
-# Arguments:
-# <mysql username>
-# <mysql password>
-# <database name>
-# <state name searched>
-
+"""Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py
+Arguments:
+<mysql username>
+<mysql password>
+<database name>
+<state name searched>
+"""
 
 import sys
 import MySQLdb
 
 db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 c = db.cursor()
-query = "SELECT * FROM `states`"
+query = "SELECT * FROM `states` WHERE `name` = '{}'".format(sys.argv[4])
 c.execute(query)
 result = c.fetchall()
 for data in result:
-    if sys.argv[4] == data[1]:
-        print(data)
+    print(data)
 c.close()
 db.close()
