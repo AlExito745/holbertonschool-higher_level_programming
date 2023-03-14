9dec5b58dc37f184e1a720f8d9464fb534f3162b
AlExito745 <alxtinoco1988@gmail.com>
2023-03-14 09:44:22 -0700

    refactor: add new line code

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    query = "SELECT * FROM `states` WHERE `name` LIKE BINARY 'N%' ORDER BY `id` ASC"
+    query = "SELECT * FROM `states`"
+    query += "WHERE `name` LIKE BINARY 'N%' ORDER BY `id` ASC"
     c.execute(query)
     result = c.fetchall()
     for data in result:
