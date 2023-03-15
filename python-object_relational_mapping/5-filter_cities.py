469a07bdd11020d54382157e81e79afc64974d7e
AlExito745 <alxtinoco1988@gmail.com>
2023-03-15 08:46:03 -0700

    feat: script that use an field as argument and list all cities in database

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Take a name of state as argument that
+lists all cities of that state using the database hbtn_0e_4_usa.
+Usage: ./5-filter_cities.py
+Arguments:
+<mysql username>
+<mysql password>
+<database name>
+<state name>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    query = "SELECT * FROM `cities` AS `c` INNER JOIN \
+        `states` AS `s`"
+    query += "ON `c`.`state_id` = `s`.`id` ORDER BY `c`.`id` ASC"
+    c.execute(query)
+    result = c.fetchall()
+    for data in result:
+        if sys.argv[4] == data[4]:
+            print("{}".format(data[2], sep=", "))
