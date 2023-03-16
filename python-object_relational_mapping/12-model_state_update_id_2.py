f6d3da2d0d89e19f751fe62754ec89e144910608
AlExito745 <alxtinoco1988@gmail.com>
2023-03-16 15:07:06 -0700

    style: remove whitespace on line 21

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     engine = create_engine(db, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     state = session.query(State).filter_by(id=2).first()
     state.name = "New Mexico"
     session.commit()
