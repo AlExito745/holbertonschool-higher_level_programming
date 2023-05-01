e33679942a00014005a52e870ed2c0f16c52b86b
AlExito745 <alxtinoco1988@gmail.com>
2023-04-30 22:27:07 -0700

    feat: update code

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
-// script that computes the number of tasks completed by user id.
+// script that gets the contents of a webpage and stores it in a file.
 
 const url = process.argv[2];
 const file = process.argv[3];
 const req = require('request');
-const fs = require('fs');
+const fileStream = require('fs');
+
 req(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    fs.writeFile(file, body, 'utf-8', (error) => {
+    fileStream.writeFile(file, body, 'utf-8', (error) => {
       if (error) console.log(error);
     });
   }
