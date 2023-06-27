f06cd3d95d5b5397b4de3965643de820391a3c70
AlExito745 <alxtinoco1988@gmail.com>
2023-06-27 11:03:01 -0500

    feat: update code

--- javascript_objects_scopes_closures/100-map.js
+++ javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-// script that imports an array and computes a new array.
 
-const originalList = require('./100-data').list;
-console.log(originalList);
-const mappedList = originalList.map (function (e, index) {
-    return (e * index);
-});
-console.log(mappedList);
+const arr = require('./100-data').list;
+
+console.log(arr);
+console.log(arr.map((x, idx) => x * idx));
