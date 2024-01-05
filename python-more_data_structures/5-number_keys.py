205e025b6ad33119b538c61520416ebe43109115
Alex Nuñez <110069281+AlExito745@users.noreply.github.com>
2024-01-04 20:59:29 -0500

    Delete python-more_data_structures directory
    Let's recreate these tasks.

--- python-more_data_structures/5-number_keys.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def number_keys(a_dictionary):
-    # return the number of keys in a dictionary
-    num_keys = len(a_dictionary.keys())
-    return(num_keys)
--- python-more_data_structures/7-update_dictionary.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    # function that update a dictionary
-    a_dictionary.update([(key, value)])
-    return(a_dictionary)
