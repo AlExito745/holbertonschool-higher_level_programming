5477dde65b85ad0af6bf6aa63463a260ec29e679
AlExito745 <alxtinoco1988@gmail.com>
2024-01-11 16:08:29 -0500

    feat: script that improve geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+# Alex Nuñez <5694@holbertonstudents.com>
+"""Define a class."""
+
+
+class BaseGeometry:
+    """Implementation of the BaseGeometry class."""
+
+    def area(self):
+        """Define a area of an object."""
+        raise Exception("area() is not implemented")
